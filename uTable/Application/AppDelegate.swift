0eab0c0eb7b03d8de6f0883f30f581617ce48bda
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2019-02-06 14:17:27 +0300

    Removed unnecessary references

---
+++ uTable/Application/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  uTable
+//
+//  Created by Vladislav Kondrashkov on 2/6/19.
+//  Copyright © 2019 Vladislav Kondrashkov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // TODO: AppCoordinator
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) { }
+
+    func applicationDidEnterBackground(_ application: UIApplication) { }
+
+    func applicationWillEnterForeground(_ application: UIApplication) { }
+
+    func applicationDidBecomeActive(_ application: UIApplication) { }
+
+    func applicationWillTerminate(_ application: UIApplication) { }
+}
+
