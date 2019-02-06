911e104a9a8120cd9b064b2c0a7f1ed96f16f15f
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2019-02-06 14:28:38 +0300

    Added standart modules

---
+++ uTable/Core/Module/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  uTable
+//
+//  Created by Vladislav Kondrashkov on 2/6/19.
+//  Copyright © 2019 Vladislav Kondrashkov. All rights reserved.
+//
+
+import Foundation
+
+protocol Coordinator: AnyObject {
+    func start()
+}
