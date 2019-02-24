f77327dec602cbec31da1dbeeb6377dba2ad6992
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2019-02-24 22:10:11 +0300

    Template ViewController with random background color

--- eStore/Scenes/Catalog/CatalogView.swift
+++ eStore/Scenes/Catalog/CatalogView.swift
@@ -56,6 +56,15 @@ extension CatalogViewImpl: UITableViewDelegate {
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let templateViewController = UIViewController()
+        let randomColors: [UIColor] = [
+            .red, .blue, .white,
+            .purple, .orange, .yellow,
+            .black, .gray, .lightGray
+        ]
+        templateViewController.view.backgroundColor = randomColors[Int.random(in: 0..<randomColors.count)]
+        templateViewController.title = categoryTableViewDataSource.categories[indexPath.row].name
+        navigationController?.pushViewController(templateViewController, animated: true)
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
