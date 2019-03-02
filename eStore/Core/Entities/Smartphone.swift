bf4d7ae63e1999864bb114b51e3b9d940dd02b5d
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2019-03-02 15:13:41 +0300

    Added conformance to Mappable protocol for Smartphone class

--- eStore/Core/Entities/Smartphone.swift
+++ eStore/Core/Entities/Smartphone.swift
@@ -6,9 +6,10 @@
 //  Copyright © 2019 Vladislav Kondrashkov. All rights reserved.
 //
 
-// TODO: Make class mappable
-class Smartphone {
-    var id: Int
+import ObjectMapper
+
+class Smartphone: ImmutableMappable {
+    var id: String
     var name: String
     var brand: Brand
     var batteryCapacity: Int
@@ -16,7 +17,7 @@ class Smartphone {
     var operatingSystem: OperatingSystem
     // Processor
 
-    init(id: Int,
+    init(id: String,
          name: String,
          brand: Brand,
          batteryCapacity: Int,
@@ -30,7 +31,22 @@ class Smartphone {
         self.display = display
         self.operatingSystem = operatingSystem
     }
+
+    required init(map: Map) throws {
+        id = try map.value("id")
+        name = try map.value("name")
+        brand = Brand(id: 0, name: "") // Temprorary
+        batteryCapacity = try map.value("batteryCapacity")
+        display = Display(width: 0, height: 0) // Temprorary
+        operatingSystem = .iOS // Temporary
+    }
+
+    func mapping(map: Map) {
+//        date >>> (map["dt_txt"], DateTransform())
+//        temperature >>> map["main.temp"]
+//        status >>> map["weather.0.description"]
+    }
 }
 
 // MARK: - StoreItem implementation
-extension Smartphone: StoreItem { }
+//extension Smartphone: StoreItem { }
--- eStore/Core/Networking/GoodsManager.swift
+++ eStore/Core/Networking/GoodsManager.swift
@@ -7,16 +7,18 @@
 //
 
 import Foundation
+import ObjectMapper
 
 protocol GoodsManager {
-    func fetchSmartphones(from json: [String: Any], completion: @escaping ([Smartphone]) -> Void)
+    func fetchSmartphones(from json: String, completion: @escaping ([Smartphone]?) -> Void)
     func fetchLaptops(from json: [String: Any], completion: @escaping ([Laptop]) -> Void)
     func fetchTV(from json: [String: Any], completion: @escaping ([TV]) -> Void)
 }
 
 class GoodsManagerImpl: GoodsManager {
-    func fetchSmartphones(from json: [String: Any], completion: @escaping ([Smartphone]) -> Void) {
-
+    func fetchSmartphones(from json: String, completion: @escaping ([Smartphone]?) -> Void) {
+        let smartphonesList = try? Mapper<Smartphone>().mapArray(JSONString: json)
+        completion(smartphonesList)
     }
 
     func fetchLaptops(from json: [String: Any], completion: @escaping ([Laptop]) -> Void) {
