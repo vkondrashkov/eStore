bdba95567bcb511966dc244ab245e0b797e580c0
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2019-03-20 23:46:19 +0300

    ESI-007 Cart module setup

---
+++ eStore/Scenes/Cart/CartView.swift
@@ -0,0 +1,34 @@
+//
+//  CartView.swift
+//  eStore
+//
+//  Created by Vladislav Kondrashkov on 3/20/19.
+//  Copyright © 2019 Vladislav Kondrashkov. All rights reserved.
+//
+
+import UIKit
+
+class CartViewImpl: UIViewController {
+
+}
+
+// MARK: - CartView implementation
+extension CartViewImpl: CartView {
+    func showActivityIndicator() { }
+
+    func hideActivityIndicator() { }
+
+    func display(storeItemList: [StoreItem]) { }
+}
+
+// MARK: - CartShow implementation
+extension CartViewImpl: CartShow {
+    var rootViewController: UIViewController {
+        return self
+    }
+}
+
+// MARK: - Constraints
+private extension CartViewImpl {
+
+}
