72b993993e854225e9b31fbf09f091444d6ca539
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2019-11-03 20:33:43 +0300

    Added spec files for BD tests

---
+++ Tests/AlertFactorySpec.swift
@@ -0,0 +1,9 @@
+//
+//  AlertFactorySpec.swift
+//  Tests
+//
+//  Created by Vladislav Kondrashkov on 11/3/19.
+//  Copyright © 2019 Vladislav Kondrashkov. All rights reserved.
+//
+
+import Foundation
