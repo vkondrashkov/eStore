babd1953f6f146000a9068856bc19d65fa51b66a
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2019-11-17 21:31:30 +0300

    Added Result extension

---
+++ eStore/Core/Utils/Result+Values.swift
@@ -0,0 +1,25 @@
+//
+//  Result+Values.swift
+//  eStore
+//
+//  Created by Vladislav Kondrashkov on 11/17/19.
+//  Copyright © 2019 Vladislav Kondrashkov. All rights reserved.
+//
+
+import Foundation
+
+extension Result {
+    var value: Success? {
+        guard case .success(let value) = self else{
+            return nil
+        }
+        return value
+    }
+
+    var error: Error? {
+        guard case .failure(let error) = self else {
+            return nil
+        }
+        return error
+    }
+}
