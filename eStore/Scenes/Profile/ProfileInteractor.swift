3c7af46a88e9904bd68094fddb8d87d67490f8f3
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2019-12-17 21:53:49 +0300

    Added interactor files

---
+++ eStore/Scenes/Profile/ProfileInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileInteractor.swift
+//  eStore
+//
+//  Created by Vladislav Kondrashkov on 12/17/19.
+//  Copyright © 2019 Vladislav Kondrashkov. All rights reserved.
+//
+
+import Foundation
